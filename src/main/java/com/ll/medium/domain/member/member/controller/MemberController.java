3620cabbba4e5a057f2f0e5d52b7e8a7e3ed75d7
jhs512 <jangka512@gmail.com>
2023-12-08 14:39:46 +0900

    JoinForm class

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.ll.medium.domain.member.member.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +15,19 @@ public class MemberController {
     @GetMapping("/join")
     public String showJoin() {
         return "domain/member/member/join";
+    }
+
+    @Setter
+    @Getter
+    public static class JoinForm {
+        private String username;
+        private String password;
+    }
+
+    @PostMapping("/join")
+    public String signup(JoinForm joinForm) {
+
+
+        return "redirect:/";
     }
 }
