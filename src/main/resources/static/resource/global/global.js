8345b256a4e5a8ae7aee4013b6ecea2561466caf
jhs512 <jangka512@gmail.com>
2023-12-08 15:44:54 +0900

    work

--- src/main/resources/static/resource/global/global.js
+++ src/main/resources/static/resource/global/global.js
@@ -1,3 +1,60 @@
+/* 유틸, 토스틀 */
+toastr.options = {
+    closeButton: true,
+    debug: false,
+    newestOnTop: true,
+    progressBar: true,
+    positionClass: "toast-top-right",
+    preventDuplicates: false,
+    onclick: null,
+    showDuration: "300",
+    hideDuration: "1000",
+    timeOut: "5000",
+    extendedTimeOut: "1000",
+    showEasing: "swing",
+    hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut"
+};
+
+function parseMsg(msg) {
+    const [pureMsg, ttl] = msg.split(";ttl=");
+
+    if ( ttl === undefined ) {
+        return [pureMsg, true];
+    };
+
+    const currentJsUnixTimestamp = new Date().getTime();
+
+    if (ttl && parseInt(ttl) < currentJsUnixTimestamp) {
+        return [pureMsg, false];
+    }
+
+    return [pureMsg, true];
+}
+
+function toastMsg(isNotice, msg) {
+    if (isNotice) toastNotice(msg);
+    else toastWarning(msg);
+}
+
+function toastNotice(msg) {
+    const [pureMsg, needToShow] = parseMsg(msg);
+
+    if (needToShow) {
+        toastr["success"](pureMsg, "알림");
+    }
+}
+
+function toastWarning(msg) {
+    const [pureMsg, needToShow] = parseMsg(msg);
+
+    if (needToShow) {
+        toastr["warning"](pureMsg, "경고");
+    }
+}
+
+/* 유틸, URL */
 function getUrlParams(urlString) {
     // 평문 쿼리 문자열을 추출합니다.
     const url = new URL(urlString);
