89a624a7caeba5296c7fb77aadf4aeffe42b89da
jhs512 <jangka512@gmail.com>
2023-12-08 15:25:56 +0900

    work

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.medium.domain.member.member.controller;
 
+import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.service.MemberService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -33,8 +34,10 @@ public class MemberController {
 
     @PostMapping("/join")
     public String join(@Valid JoinForm joinForm) {
-        memberService.join(joinForm.getUsername(), joinForm.getPassword());
+        Member member = memberService.join(joinForm.getUsername(), joinForm.getPassword());
 
-        return "redirect:/";
+        long id = member.getId();
+
+        return "redirect:/?msg=No %d member joined.".formatted(id);
     }
 }
