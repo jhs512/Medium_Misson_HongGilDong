5bf3fafbe889b05b1b97f51baaf4400294299e11
jhs512 <jangka512@gmail.com>
2023-12-08 18:58:27 +0900

    work 7

--- src/main/java/com/ll/medium/global/init/NotProd.java
+++ src/main/java/com/ll/medium/global/init/NotProd.java
@@ -1,15 +1,21 @@
 package com.ll.medium.global.init;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class NotProd {
     @Bean
     public ApplicationRunner initNotProd() {
-        return args -> {
-            System.out.println("NotProd");
+        return new ApplicationRunner() {
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                log.debug("NotProd 실행됨");
+            }
         };
     }
 }
