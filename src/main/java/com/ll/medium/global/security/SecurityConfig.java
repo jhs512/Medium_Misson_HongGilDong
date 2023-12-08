c509fcc4fbb2672f960d16b73bd77e9f02678c7c
jhs512 <jangka512@gmail.com>
2023-12-08 19:46:15 +0900

    work 18

--- src/main/java/com/ll/medium/global/security/SecurityConfig.java
+++ src/main/java/com/ll/medium/global/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ll.medium.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,6 +12,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
