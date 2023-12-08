d631924190e02f4b2ab8bc6972592ff0ee1a9df1
jhs512 <jangka512@gmail.com>
2023-12-08 16:18:28 +0900

    work

--- src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
+++ src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -14,9 +16,17 @@ public class MemberService {
 
     @Transactional
     public Member join(String username, String password) {
+        if (findByUsername(username).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+
         Member member = new Member(username, password);
         memberRepository.save(member);
 
         return member;
+    }
+
+    private Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
     }
 }
