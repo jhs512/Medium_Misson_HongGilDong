ff07a027bd5916ded13b7c403ee97b2ec5f7186a
jhs512 <jangka512@gmail.com>
2023-12-08 19:30:49 +0900

    work 12

--- src/main/java/com/ll/medium/global/init/All.java
+++ src/main/java/com/ll/medium/global/init/All.java
@@ -6,24 +6,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 
 @Configuration
-@Profile("!prod")
 @Slf4j
 @RequiredArgsConstructor
-public class NotProd {
+public class All {
     private final MemberService memberService;
 
     @Bean
-    @Order(3)
-    public ApplicationRunner initNotProd() {
+    @Order(2)
+    public ApplicationRunner initAll() {
         return args -> {
-            if (memberService.count() > 2) return;
+            if (memberService.count() > 0) return;
 
-            memberService.join("user1", "1234");
-            memberService.join("user2", "1234");
+            memberService.join("system", "1234");
+            memberService.join("admin", "1234");
         };
     }
 }
