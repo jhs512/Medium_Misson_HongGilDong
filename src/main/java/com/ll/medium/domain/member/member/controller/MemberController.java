6ecd0650b776424a4e5e8750f5f73d5e5eb1afb4
jhs512 <jangka512@gmail.com>
2023-12-08 14:40:22 +0900

    JoinForm class 2

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.ll.medium.domain.member.member.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -20,12 +22,14 @@ public class MemberController {
     @Setter
     @Getter
     public static class JoinForm {
+        @NotBlank
         private String username;
+        @NotBlank
         private String password;
     }
 
     @PostMapping("/join")
-    public String signup(JoinForm joinForm) {
+    public String signup(@Valid JoinForm joinForm) {
 
 
         return "redirect:/";
