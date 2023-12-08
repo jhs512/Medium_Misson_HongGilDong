aec0b8a011cf05aefe2f95dd525b68a3b11ca19c
jhs512 <jangka512@gmail.com>
2023-12-08 20:54:04 +0900

    work 30

--- src/main/java/com/ll/medium/global/rq/Rq/Rq.java
+++ src/main/java/com/ll/medium/global/rq/Rq/Rq.java
@@ -4,11 +4,16 @@ import com.ll.medium.global.rsData.RsData.RsData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @Component
 @RequestScope
@@ -43,5 +48,31 @@ public class Rq {
         if (rs.isFail()) return historyBack(rs.getMsg());
 
         return redirect(path, rs.getMsg());
+    }
+
+    public User getUser() {
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .map(Authentication::getPrincipal)
+                .filter(it -> it instanceof User)
+                .map(it -> (User) it)
+                .orElse(null);
+    }
+
+    public boolean isLogin() {
+        return getUser() != null;
+    }
+
+    public boolean isLogout() {
+        return !isLogin();
+    }
+
+    public boolean isAdmin() {
+        if (isLogout()) return false;
+
+        return getUser()
+                .getAuthorities()
+                .stream()
+                .anyMatch(it -> it.getAuthority().equals("ROLE_ADMIN"));
     }
 }
--- src/main/java/com/ll/medium/global/security/SecurityConfig.java
+++ src/main/java/com/ll/medium/global/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -54,6 +55,11 @@ public class SecurityConfig {
                                         .loginPage("/member/login")
                                         .defaultSuccessUrl("/?msg=" + URLEncoder.encode("환영합니다.", StandardCharsets.UTF_8))
                                         .failureUrl("/member/login?failMsg=" + URLEncoder.encode("아이디 또는 비밀번호가 틀렸습니다.", StandardCharsets.UTF_8))
+                )
+                .logout(
+                        logout ->
+                                logout
+                                        .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
                 );
 
         return http.build();
