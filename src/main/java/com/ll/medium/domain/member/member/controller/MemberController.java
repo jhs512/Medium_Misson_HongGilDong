b689df408d693e803ef47f0bcd2e8ae80ec4b150
jhs512 <jangka512@gmail.com>
2023-12-08 16:12:51 +0900

    work

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -2,24 +2,23 @@ package com.ll.medium.domain.member.member.controller;
 
 import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.service.MemberService;
+import com.ll.medium.global.rq.Rq.Rq;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.apache.catalina.util.URLEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.nio.charset.StandardCharsets;
 
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final Rq rq;
 
     @GetMapping("/join")
     public String showJoin() {
@@ -39,8 +38,9 @@ public class MemberController {
     public String join(@Valid JoinForm joinForm) {
         Member member = memberService.join(joinForm.getUsername(), joinForm.getPassword());
 
-        String msg = "%s님 환영합니다. 회원가입이 완료되었습니다. 로그인 후 이용해주세요.".formatted(member.getUsername());
-        msg = new URLEncoder().encode(msg, StandardCharsets.UTF_8);
-        return "redirect:/?msg=" + msg;
+        return rq.redirect(
+                "/",
+                "%s님 환영합니다. 회원가입이 완료되었습니다. 로그인 후 이용해주세요.".formatted(member.getUsername())
+        );
     }
 }
---
+++ src/main/java/com/ll/medium/global/rq/Rq/Rq.java
@@ -0,0 +1,34 @@
+package com.ll.medium.global.rq.Rq;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Component
+@RequestScope
+@RequiredArgsConstructor
+public class Rq {
+    private final HttpServletRequest request;
+    private final HttpServletResponse response;
+
+    public String redirect(String url, String msg) {
+        msg = URLEncoder.encode(msg, StandardCharsets.UTF_8);
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("redirect:");
+        sb.append(url);
+
+        if (msg != null) {
+            sb.append("?msg=");
+            sb.append(msg);
+        }
+
+        return sb.toString();
+    }
+}
